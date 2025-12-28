54fce40a97e1d83703c5b437f6a401811b5302f8
someone364 <nurgozhindaryn@gmail.com>
2025-12-28 18:53:49 +0500

    Main.java

--- src/Ass2/Daryn/Main.java
+++ src/Ass2/Daryn/Main.java
@@ -6,24 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 7. Объекты с разными типами ссылок
 
-        // Ссылка типа Human
         Human empAsHuman = new Employee(32, "Aidar Kenzhebek", false, "Developer", 150000f);
 
-        // Ссылка типа интерфейса IWorkable
         IWorkable worker = new Employee(28, "Ainur Mukhamadi", true, "HR Manager", 120000f);
 
-        // Ссылка типа интерфейса ILearnable
         ILearnable learner = new Student(20, "Dana Talgat", false, "Computer Science", 4.8f, "STU001");
 
-        // Ссылка типа самого класса
         Employee regularEmp = new Employee(35, "Baurzhan Ali", true, "Team Lead", 200000f);
         Student regularStudent = new Student(22, "Kamila Nur", false, "Economics", 4.5f, "STU002");
 
         System.out.println("\n=== Demonstration of all methods ===\n");
 
-        // Вызов методов через разные ссылки
         empAsHuman.printBasicInfo();
         empAsHuman.introduce();
         empAsHuman.displayRoleInfo();
@@ -42,7 +36,7 @@ public class Main {
         regularStudent.displayRoleInfo();
         regularStudent.study();
         regularStudent.passExam("Statistics");
-        regularStudent.graduate();  // final метод
+        regularStudent.graduate(); 
 
         System.out.println("\nTotal employees created: " + Employee.getEmployeeCount());
 
